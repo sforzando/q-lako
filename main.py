ed5a644db36fe5f8ea17368c92244070a86ff82d
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-12-05 12:43:03 +0900

    :wrench: Refactoring code for search and registration on main.py

--- main.py
+++ main.py
@@ -9,30 +9,38 @@ from airtable_client import AirtableClient
 from flash_message import FlashMessage, FlashCategories
 
 
+def clear_session():
+    session.pop("keyword", None)
+    session.pop("product_list", None)
+    session.pop("product", None)
+
+
 @app.route("/", methods=["GET"])
 def index():
     app.logger.info("index(): GET /")
+    clear_session()
     return render_template("index.html")
 
 
 @app.route("/search", methods=["GET"])
 def search():
     app.logger.info(f"search(): GET {request.full_path}")
-    keyword = request.args.get('query', None)
-    context_dict = {
-        "subtitle": f"Search results for {keyword}",
-        "keyword": keyword
-    }
-    if not keyword:
+    session["keyword"] = request.args.get('query', None)
+
+    if not session.get("keyword", None):
         return FlashMessage.show_with_redirect("Enter any keywords.", FlashCategories.WARNING, url_for("index"))
-    else:
-        try:
-            product_list = amazon_api_client.search_products(keywords=keyword)
-            session["product_list"] = product_list if product_list else []
-            return render_template("search.html", **context_dict)
-        except AmazonException as ae:
-            app.logger.error(ae)
-            return FlashMessage.show_with_redirect(f"Error occurred. {ae}", FlashCategories.ERROR, url_for("index"))
+
+    context_dict = {
+        "subtitle": f"Search results for {session['keyword']}",
+        "keyword": session["keyword"]
+    }
+    try:
+        product_list = amazon_api_client.search_products(keywords=session["keyword"])
+        session["product_list"] = product_list if product_list else []
+        return render_template("search.html", **context_dict)
+    except AmazonException as ae:
+        app.logger.error(ae)
+        return FlashMessage.show_with_redirect(f"Error occurred. {ae}", FlashCategories.ERROR, url_for("index"))
 
 
 @ app.route("/registration", methods=["GET", "POST"])
@@ -40,34 +48,43 @@ def registration():
     if request.method == "GET":
         app.logger.info(f"registration: GET {request.full_path}")
         return FlashMessage.show_with_redirect("Enter any keywords.", FlashCategories.WARNING, url_for("index"))
-    else:
-        app.logger.info(f"registration: POST {request.form}")
-        app.logger.debug(f"{request.form=}")
-    context_dict = {
-        "subtitle": "a service that displays detailed information about the item."
-    }
-    asin = request.form.get("asin", "")
-    if asin:
-        for product in session["product_list"]:
-            if product.asin == asin:
-                session["asset"] = product
-                if product.info.contributors:
-                    product.info.contributors = ",".join(
-                        [contributor.name for contributor in product.info.contributors])
-                if product.product.features:
-                    product.product.features = ",".join(product.product.features)
 
-                context_dict["product"] = product
+    app.logger.info("registration: POST /registration")
+    app.logger.debug(f"{request.form=}")
+    asin = request.form.get("asin", "")
+
+    if not asin or not session.get("product_list", None):
+        return FlashMessage.show_with_redirect(
+            "Please try the procedure again from the beginning, sorry for the inconvenience.",
+            FlashCategories.WARNING,
+            url_for("index"))
+
+    context_dict = {}
+    session.pop("product", None)
+    for product in session["product_list"]:
+        if product.asin == asin:
+            if product.info.contributors:
+                product.info.contributors = ",".join(
+                    [contributor.name for contributor in product.info.contributors])
+            if product.product.features:
+                product.product.features = ",".join(product.product.features)
+            context_dict["subtitle"] = f"Registration for details of {product.title}"
+            session["product"] = product
+
+    if session.get("product", None):
+        return render_template("registration.html", **context_dict)
     else:
-        return render_template("index.html")
-    return render_template("registration.html", **context_dict, asset=session["product_list"])
+        return FlashMessage.show_with_redirect(
+            "Please try the procedure again from the beginning, sorry for the inconvenience.",
+            FlashCategories.WARNING,
+            url_for("index"))
 
 
 @app.route("/register_airtable", methods=["POST"])
 def register_airtable():
     app.logger.info("register_airtable(): POST /register_airtable")
     app.logger.debug(f"{request.form=}")
-    posted_asset = request.form.to_dict()
+    posted_asset = request.form.to_dict() if request.form else {}
     if posted_asset:
         registrable_asset = Asset(
             title=posted_asset["title"],
@@ -88,7 +105,7 @@ def register_airtable():
         return FlashMessage.show_with_redirect("Registration completed!", FlashCategories.INFO, url_for("index"))
     else:
         context_dict = {
-            "product": session.get("asset", "")
+            "subtitle": posted_asset.get("title", None)
         }
         app.logger.debug(f"{context_dict}=")
         return FlashMessage.show_with_render_template("Registration failed.", FlashCategories.ERROR,
