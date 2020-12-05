260380f82b48de9914d86e43218dbddd181f5cb8
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-12-05 16:26:16 +0900

    :wrench: Fix layout for registration.html

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ def search():
         return FlashMessage.show_with_redirect("Enter any keywords.", FlashCategories.WARNING, url_for("index"))
 
     context_dict = {
-        "subtitle": f"Search results for {session['keyword']}",
-        "keyword": session["keyword"]
+        "subtitle": f"Search results for {session.get('keyword', None)}",
+        "keyword": session.get("keyword", None)
     }
     try:
-        product_list = amazon_api_client.search_products(keywords=session["keyword"])
+        product_list = amazon_api_client.search_products(keywords=session.get("keyword", None))
         session["product_list"] = product_list if product_list else []
         return render_template("search.html", **context_dict)
     except AmazonException as ae:
@@ -64,10 +64,11 @@ def registration():
     for product in session["product_list"]:
         if product.asin == asin:
             if product.info.contributors:
-                product.info.contributors = ",".join(
+                product.info.contributors = ", ".join(
                     [contributor.name for contributor in product.info.contributors])
             if product.product.features:
-                product.product.features = ",".join(product.product.features)
+                print(f"{product.product.features=}")
+                product.product.features = "\n".join(product.product.features)
             context_dict["subtitle"] = f"Registration for details of {product.title}"
             session["product"] = product
 
@@ -84,22 +85,26 @@ def registration():
 def register_airtable():
     app.logger.info("register_airtable(): POST /register_airtable")
     app.logger.debug(f"{request.form=}")
-    posted_asset = request.form.to_dict() if request.form else {}
+    posted_asset = request.form.to_dict() if request.form else{}
+
+    def convert_str_none_int_0(n: str) -> int:
+        return 0 if n == "None" else n
+
     if posted_asset:
         registrable_asset = Asset(
-            title=posted_asset["title"],
-            asin=posted_asset["asin"],
-            url=posted_asset["url"],
-            images=[{"url": posted_asset["image_url"]}],
-            manufacture=posted_asset["manufacturer"],
-            contributor=posted_asset["contributors"],
-            product_group=posted_asset["product_group"],
-            publication_date=0 if posted_asset["publication_date"] else 0,
-            features=posted_asset["features"],
-            default_position=posted_asset["default_positions"],
-            current_position=posted_asset["current_positions"],
-            note=posted_asset["note"],
-            registrant_name=posted_asset["registrants_name"])
+            title=posted_asset.get("title", None),
+            asin=posted_asset.get("asin", None),
+            url=posted_asset.get("url", None),
+            images=[{"url": posted_asset.get("image_url", None)}],
+            manufacture=posted_asset.get("manufacturer", None),
+            contributor=posted_asset.get("contributors", None),
+            product_group=posted_asset.get("product_group", None),
+            publication_date=convert_str_none_int_0(posted_asset.get("publication_date")),
+            features=posted_asset.get("features", None),
+            default_position=posted_asset.get("default_positions", None),
+            current_position=posted_asset.get("current_positions", None),
+            note=posted_asset.get("note", None),
+            registrant_name=posted_asset.get("registrants_name", None))
         AirtableClient().register_asset(registrable_asset)
         app.logger.info(f"Registration completed! {registrable_asset=}")
         return FlashMessage.show_with_redirect("Registration completed!", FlashCategories.INFO, url_for("index"))
