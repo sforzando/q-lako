fedb00a73679ab8b7968dde7f19386b633887b83
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-12-05 12:38:47 +0900

    :100: Update test for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,60 +20,50 @@ def test_GET_index(test_client):
 
 def test_GET_search_with_correct_query(test_client):
     response = test_client.get("/search?query=kindle")
-    assert b"kindle" in response.data
-
-
-def test_GET_search_with_using_session(test_client):
-    test_client.get("/search?query=kindle")
-    response = test_client.get("/search?query=kindle")
-    assert b"kindle" in response.data
+    assert b"Search results for kindle" in response.data
 
 
 def test_GET_search_with_incorrect_query(test_client):
     response = test_client.get("/search?unexpected_query=kindle", follow_redirects=True)
+    assert b"Registration of equipment and books." in response.data
     assert b"Enter any keywords." in response.data
 
 
 def test_GET_search_with_not_inputted_query(test_client):
     response = test_client.get("/search?query=", follow_redirects=True)
-    assert b"a service to quickly register equipments and books." in response.data
+    assert b"Registration of equipment and books." in response.data
     assert b"Enter any keywords." in response.data
 
 
 def test_GET_search_direct_access(test_client):
     response = test_client.get("/search", follow_redirects=True)
+    assert b"Registration of equipment and books." in response.data
     assert b"Enter any keywords." in response.data
 
 
 def test_GET_registration_direct_access(test_client):
     response = test_client.get("/registration", follow_redirects=True)
-    assert b"a service to quickly register equipments and books." in response.data
+    assert b"Registration of equipment and books." in response.data
     assert b"Enter any keywords." in response.data
 
 
-def test_GET_registration_asin(test_client):
+def test_POST_registration_success(test_client):
     test_client.get("/search?query=サーカスTC")
     response = test_client.post("/registration", data={"asin": "B07XB5WX89"})
-    assert b"B07XB5WX89" in response.data
+    assert "Registration for details of テンマクデザイン サーカス TC DX" in response.data.decode("UTF-8")
 
 
-def test_GET_registration_contributors(test_client):
+def test_POST_registration_failure(test_client):
+    response = test_client.post("/registration", follow_redirects=True)
+    assert b"Registration of equipment and books." in response.data
+    assert b"Please try the procedure again from the beginning, sorry for the inconvenience." in response.data
+
+
+def test_POST_registration_contributors(test_client):
     test_client.get("/search?query=DeepLearning")
     response = test_client.post("/registration", data={"asin": "4873117585"})
-    assert b"4873117585" in response.data
-
-
-def test_GET_registration_asin_no(test_client):
-    test_client.get("/search?query=サーカスTC")
-    response = test_client.post("/registration", data={"asin": ""})
-    assert b"a service to quickly register equipments and books." in response.data
-
-
-def test_POST_register_airtable_no_asin(test_client):
-    test_client.get("/search?query=サーカスTC")
-    test_client.post("/registration", data={"asin": "B07XB5WX89"})
-    response = test_client.post("/register_airtable")
-    assert "サーカスTC" in response.data.decode("utf-8")
+    assert "ゼロから作るDeep Learning ―Pythonで学ぶディープラーニングの理論と実装" in response.data.decode("UTF-8")
+    assert "斎藤 康毅" in response.data.decode("UTF-8")
 
 
 def test_POST_register_airtable_success(test_client):
@@ -98,3 +88,10 @@ def test_POST_register_airtable_success(test_client):
     test_client.post("/registration", data={"asin": "B07XB5WX89"})
     response = test_client.post("/register_airtable", data=imd, follow_redirects=True)
     assert b"Registration completed!" in response.data
+
+
+def test_POST_register_airtable_failure(test_client):
+    test_client.get("/search?query=サーカスTC")
+    test_client.post("/registration", data={"asin": "B07XB5WX89"})
+    response = test_client.post("/register_airtable", data={}, follow_redirects=True)
+    assert b"Registration failed." in response.data
