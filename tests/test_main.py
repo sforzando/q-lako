765e6425e8c6b8233df617b2ebde9f75de504c38
yusuke WATANABE <yusuke@sforzando.co.jp>
2020-12-09 22:17:37 +0900

    :+1: Changed the title name to be generic

--- tests/test_main.py
+++ tests/test_main.py
@@ -88,7 +88,7 @@ def test_POST_register_airtable_success(test_client):
     test_client.post("/registration", data={"asin": "B07XB5WX89"})
     response = test_client.post("/register_airtable", data=imd, follow_redirects=True)
     assert b"Registration completed!" in response.data
-    AirtableClient().delete_asset("title", "テンマクデザイン サーカス TC DX")
+    AirtableClient().delete_asset("title", imd.getlist(key="title")[0])
 
 
 def test_POST_register_airtable_failure(test_client):
